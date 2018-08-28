e27974345ddb79a3b9a48ccdfa3ed1ddd5a200b6
briansbotlab <37006912+briansbotlab@users.noreply.github.com>
2018-08-28 21:22:21 +0800

    Update 20180806.py

--- python_project/20180806.py
+++ python_project/20180806.py
@@ -10,7 +10,7 @@ from bs4 import BeautifulSoup
 import json
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'}
-#name = input('Input Product Name:')
+
 
 url = 'http://poedb.tw/xyz.php?name='+'獸腹'+'&league=Warbands'
 
@@ -20,7 +20,6 @@ html=res.content
 
 soup = BeautifulSoup(html,'lxml')
 
-#al = soup.find_all('span',class_='sortable', attrs={'data-name': 'price','data-value':'value'})
 al = soup.find_all('span')
 
 for item in al:
@@ -31,22 +30,4 @@ for item in al:
 
 
 
-#data = json.loads(res.text)
 
-
-#print(name)
-
-'''
-webdatas = data['prods']
-
-
-print('查詢筆數:' + str(len(webdatas)))
-print('______________________________')
-
-for product in webdatas:
-    print('產品名稱:' + product['name'])
-    print('產品價格:' + str(product['price']))
-    print('購買網址:' + 'https://24h.pchome.com.tw/prod/' + product['Id'])
-    print('- - - - - - - - - - - - - - - - - - - - - - -')
-    
-    '''
